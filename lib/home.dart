94a9dcfda560cdc0d43b14861297d6345d3dde0e
Faisal-77 <76266938+Faisal-77@users.noreply.github.com>
2024-01-30 07:09:49 +0300

    Faisal: The New Version

--- lib/home.dart
+++ lib/home.dart
@@ -13,7 +13,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   SqlDb sqlDb = SqlDb();
-
+  int selectedIndex = -1;
   Future<List<Map>> readData() async {
     List<Map> response = await sqlDb.readDate("SELECT * FROM dogs");
     return response;
@@ -23,77 +23,161 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(
-            child: Text(
-          "Dog Database",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
-        )),
-        backgroundColor: const Color.fromRGBO(98, 0, 238, 1),
-        elevation: 5,
+        centerTitle: true,
+        title: const Text('Dogs Database'
+        ,style: TextStyle(fontWeight: FontWeight.w800,color: Colors.white)),
+        backgroundColor: Colors.deepPurple,
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Color.fromRGBO(1, 135, 134, 0.3),
-        ),
-        child: FutureBuilder(
-          builder: (BuildContext context, AsyncSnapshot<List<Map>> snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    elevation: 5,
-                    margin: const EdgeInsets.all(8),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                          backgroundColor: Color.fromRGBO(98, 0, 238, 1),
-                          child: IconButton(
-                              onPressed: () => _showDetails(
-                                    snapshot.data![index]['dogname'],
-                                    snapshot.data![index]['dogage'],
-                                  ),
-                              icon: Icon(Icons.pets, color: Colors.white))),
-                      title: IconButton(
-                        onPressed: () =>
-                            _deleteDog(snapshot.data![index]['id']),
-                        icon: const Icon(Icons.delete, color: Colors.red),
-                      ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            onPressed: () => _editDog(
-                              snapshot.data![index]['id'],
-                              snapshot.data![index]['dogname'],
-                              snapshot.data![index]['dogage'],
-                            ),
-                            icon: const Icon(Icons.edit,
-                                color: Color.fromRGBO(187, 134, 252, 1)),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            TextField(
+              controller: dogName,
+              decoration: const InputDecoration(
+                  hintText: 'Dog Name',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ))),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: dogAge,
+              keyboardType: TextInputType.number,
+              maxLength: 2,
+              decoration: const InputDecoration(
+                  hintText: 'Dog Age',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ))),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      //
+                      String name = dogName.text.trim();
+                      String age = dogAge.text.trim();
+                      if (name.isNotEmpty && age.isNotEmpty) {
+                        setState(() {
+                          sqlDb.insertDate(
+                            "INSERT INTO 'dogs' ('dogname', 'dogage') VALUES ('$name', '$age')",
+                          );
+                        });
+                        dogName.clear();
+                        dogAge.clear();
+                      }
+                      //
+                    },
+                    child: const Text('Add')),
+                ElevatedButton(
+                  onPressed: () {
+                    String name = dogName.text.trim();
+                    String age = dogAge.text.trim();
+                    int id = selectedIndex;
+                    if (name.isNotEmpty && age.isNotEmpty) {
+                      setState(() {
+                        sqlDb.updateDate(
+                            "UPDATE dogs SET dogage = '$age', dogname = '$name' WHERE id = $id");
+                        dogName.clear();
+                        dogAge.clear();
+                      });
+                    }
+                  },
+                  child: const Text('Update'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: FutureBuilder(
+                builder:
+                    (BuildContext context, AsyncSnapshot<List<Map>> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else if (snapshot.hasError) {
+                        return const Center(
+                          child: Text('Error loading data'),
+                        );
+                      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                        return const Center(
+                          child: Text('No dogs found...',style: TextStyle(fontSize: 28,color: Colors.grey),),
+                        );
+                      } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          elevation: 5,
+                          margin: const EdgeInsets.all(8),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6.0),
                           ),
-                        ],
-                      ),
-                    ),
+                          child: ListTile(
+                            leading: CircleAvatar(
+                                backgroundColor: Color.fromRGBO(98, 0, 238, 1),
+                                child: IconButton(
+                                    onPressed: () => _showDetails(
+                                          snapshot.data![index]['dogname'],
+                                          snapshot.data![index]['dogage'],
+                                        ),
+                                    icon:
+                                        Icon(Icons.pets, color: Colors.white))),
+                            title: Text( snapshot.data![index]['dogname'], style: TextStyle(fontSize: 20),),
+                            subtitle: Text(snapshot.data![index]['dogage'],),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    dogName.text =
+                                        snapshot.data![index]['dogname'];
+                                    dogAge.text =
+                                        snapshot.data![index]['dogage'];
+                                    setState(() {
+                                      selectedIndex =
+                                          snapshot.data![index]['id'];
+                                    });
+                                  },
+                                  icon: const Icon(Icons.edit,
+                                      color: Color.fromRGBO(187, 134, 252, 1)),
+                                ),
+                                IconButton(
+                                  onPressed: () =>
+                                      _deleteDog(snapshot.data![index]['id']),
+                                  icon: const Icon(Icons.delete, color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
                   );
                 },
-              );
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-          future: readData(),
+                future: readData(),
+              ),
+            )
+          ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: CustomFloatingActionButton(
-        onPressed: () {
-          _addNewDog();
-        },
-      ),
     );
+  }
+
+  Future _deleteDog(int id) async {
+    setState(() {
+      sqlDb.deleteDate("DELETE FROM 'dogs' WHERE id = $id");
+    });
   }
 
   Future _showDetails(dogName, dogAge) {
@@ -113,155 +197,5 @@ class _HomeState extends State<Home> {
                   child:
                       Text("Dog Age: $dogAge", style: TextStyle(fontSize: 22))),
             ])));
-  }
-
-  Future _addNewDog() => showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: const Text("Add new dog"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 15.0),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Dog Name",
-                    filled: true,
-                    fillColor: Color.fromRGBO(211, 211, 211, 1),
-                  ),
-                  controller: dogName,
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 15.0),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Dog Age",
-                    filled: true,
-                    fillColor: Color.fromRGBO(211, 211, 211, 1),
-                  ),
-                  controller: dogAge,
-                ),
-              ),
-            ],
-          ),
-          actions: [
-            MaterialButton(
-              color: const Color.fromRGBO(187, 134, 252, 1),
-              onPressed: () {
-                String dogName1 = dogName.text;
-                String dogAge1 = dogAge.text;
-                setState(() {
-                  sqlDb.insertDate(
-                    "INSERT INTO 'dogs' ('dogname', 'dogage') VALUES ('$dogName1', '$dogAge1')",
-                  );
-                });
-                dogName.clear();
-                dogAge.clear();
-                Navigator.of(context).pop();
-              },
-              child: const Text("Insert Data"),
-            ),
-          ],
-        ),
-      );
-
-  Future _editDog(int id, String currentName, String currentAge) {
-    dogName.text = currentName;
-    dogAge.text = currentAge;
-
-    return showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text("Edit Dog"),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 15.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Dog Name",
-                  filled: true,
-                  fillColor: Color.fromRGBO(211, 211, 211, 1),
-                ),
-                controller: dogName,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 15.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Dog Age",
-                  filled: true,
-                  fillColor: Color.fromRGBO(211, 211, 211, 1),
-                ),
-                controller: dogAge,
-              ),
-            ),
-          ],
-        ),
-        actions: [
-          MaterialButton(
-            color: const Color.fromRGBO(187, 134, 252, 1),
-            onPressed: () {
-              String dogName1 = dogName.text;
-              String dogAge1 = dogAge.text;
-              setState(() {
-                sqlDb.updateDate(
-                  "UPDATE dogs SET 'dogage' = '$dogAge1' ,'dogname' = '$dogName1' WHERE id = $id",
-                );
-              });
-
-              dogName.clear();
-              dogAge.clear();
-              Navigator.of(context).pop();
-            },
-            child: const Text("Update Data"),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future _deleteDog(int id) async {
-    setState(() {
-      sqlDb.deleteDate("DELETE FROM 'dogs' WHERE id = $id");
-    });
-  }
-}
-
-class CustomFloatingActionButton extends StatelessWidget {
-  final VoidCallback onPressed;
-
-  const CustomFloatingActionButton({Key? key, required this.onPressed})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton.extended(
-      onPressed: onPressed,
-      label: const Icon(
-        Icons.add,
-      ),
-      backgroundColor: const Color.fromRGBO(187, 134, 252, 1),
-      shape: const CircleBorder(),
-    );
-  }
-}
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
-    );
   }
 }
